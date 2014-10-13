d5a299d36a676b775a9467b3b7cb0ae9223b4c9a
Joao <joaoa.comp@gmail.com>
2014-10-13 10:59:49 -0300

    Update layout list of products

--- config/application.rb
+++ config/application.rb
@@ -1,4 +1,4 @@
-require File.expand_path('../boot', __FILE__)
+    require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module Depot
   class Application < Rails::Application
+    config.assets.enabled = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
