ebef64abad10f76fafce248a3a19b4dc45a3cd78
Joao <joaoa.comp@gmail.com>
2014-11-04 15:14:41 -0200

    Cart with AJAX

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -24,24 +24,23 @@ class LineItemsController < ApplicationController
   # POST /line_items
   # POST /line_items.json
  def create
-	@cart = current_cart
-	product = Product.find(params[:product_id])
-	@line_item = @cart.add_product(product.id)
-	
-	respond_to do |format|
-		if @line_item.save
-			format.html { redirect_to(@line_item.cart,
-			  :notice => 'Line item was successfully created.' ) }
-			format.xml { render :xml => @line_item,
-			  :status => :created, :location => @line_item }
-		else
-			format.html { render :action => "new" }
-			format.xml { render :xml => @line_item.errors,
-			  :status => :unprocessable_entity }
-		end
-	end
+  @cart = current_cart
+  product = Product.find(params[:product_id])
+  @line_item = @cart.add_product(product.id)
+  respond_to do |format|
+    if @line_item.save
+      format.html { redirect_to(store_url) }
+      format.js
+      format.xml { render :xml => @line_item,
+        :status => :created, :location => @line_item }
+    else
+      format.html { render :action => "new" }
+      format.xml { render :xml => @line_item.errors,
+        :status => :unprocessable_entity }
+    end
+  end
  end
-
+  
   # PATCH/PUT /line_items/1
   # PATCH/PUT /line_items/1.json
   def update
